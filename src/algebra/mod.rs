bfeaa39ea20218d9e5cd48bfcae1107896c2e864
Kamil Duda <kamilduda01@gmail.com>
2020-07-08 21:58:29 +0200

    Add algebra module with sigmoid and exp for nd arrays

---
+++ src/algebra/mod.rs
@@ -0,0 +1,106 @@
+use ndarray::prelude::*;
+
+pub struct Algebra;
+
+impl Algebra {
+    /// Calculates Sigmoid value for each element of an array.
+    pub fn sigmoid(array: ArrayD<f32>) -> ArrayD<f32> {
+        array.mapv(|x| 1.0 / (1.0 + f32::exp(-x)))
+    }
+
+    /// Calculates exponential (e^x) for each element of an array.
+    pub fn exp(array: ArrayD<f32>) -> ArrayD<f32> {
+        array.mapv(f32::exp)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn calculates_sigmoid_for_1d_array() {
+        let xs = array![-5., -4., -3., -2., -1., 0., 1., 2., 3., 4., 5., 6.].into_dyn();
+
+        let actual = Algebra::sigmoid(xs);
+
+        let expected = array![
+            0.006692851,
+            0.01798621,
+            0.047425874,
+            0.11920292,
+            0.26894143,
+            0.5,
+            0.7310586,
+            0.880797,
+            0.95257413,
+            0.98201376,
+            0.9933072,
+            0.9975274
+        ].into_dyn();
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn calculates_sigmoid_for_2d_array() {
+        let xs = array![[-5., -4., -3., -2., -1., 0.], [1., 2., 3., 4., 5., 6.]].into_dyn();
+
+        let actual = Algebra::sigmoid(xs);
+
+        let expected = array![
+            [
+                0.006692851,
+                0.01798621,
+                0.047425874,
+                0.11920292,
+                0.26894143,
+                0.5
+            ],
+            [0.7310586, 0.880797, 0.95257413, 0.98201376, 0.9933072, 0.9975274]
+        ].into_dyn();
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn calculates_exp_for_1d_array() {
+        let xs = array![-5., -4., -3., -2., -1., 0., 1., 2., 3., 4., 5., 6.].into_dyn();
+
+        let actual = Algebra::exp(xs);
+
+        let expected = array![
+            0.006737947,
+            0.01831564,
+            0.049787067,
+            0.13533528,
+            0.36787945,
+            1.0,
+            2.7182817,
+            7.389056,
+            20.085537,
+            54.59815,
+            148.41316,
+            403.4288
+        ].into_dyn();
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn calculates_exp_for_2d_array() {
+        let xs = array![[-5., -4., -3., -2., -1., 0.], [1., 2., 3., 4., 5., 6.]].into_dyn();
+
+        let actual = Algebra::exp(xs);
+
+        let expected = array![
+            [
+                0.006737947,
+                0.01831564,
+                0.049787067,
+                0.13533528,
+                0.36787945,
+                1.0,
+            ],
+            [2.7182817, 7.389056, 20.085537, 54.59815, 148.41316, 403.4288]
+        ].into_dyn();
+        assert_eq!(actual, expected);
+    }
+}
