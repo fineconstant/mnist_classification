26c7994eee4a2ab7525ff266801cc612084971c4
Kamil Duda <kamilduda01@gmail.com>
2020-07-11 14:51:22 +0200

    Add sigmoid' function

--- src/algebra/mod.rs
+++ src/algebra/mod.rs
@@ -11,6 +11,14 @@ impl Algebra {
         array.mapv(|x| 1.0 / (1.0 + f32::exp(-x)))
     }
 
+    /// Calculates first derivative Sigmoid value for each element of an array.
+    pub fn sigmoid_prime<D>(array: Array<f32, D>) -> Array<f32, D>
+    where
+        D: Dimension,
+    {
+        Algebra::sigmoid(array).mapv(|x| x * (1.0 - x))
+    }
+
     /// Calculates exponential (e^x) for each element of an array.
     pub fn exp<D>(array: Array<f32, D>) -> Array<f32, D>
     where
@@ -63,6 +71,56 @@ mod tests {
                 0.5
             ],
             [0.7310586, 0.880797, 0.95257413, 0.98201376, 0.9933072, 0.9975274]
+        ];
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn calculates_sigmoid_prime_for_1d_array() {
+        let xs = array![-5., -4., -3., -2., -1., 0., 1., 2., 3., 4., 5., 6.];
+
+        let actual = Algebra::sigmoid_prime(xs);
+
+        let expected = array![
+            0.0066480567,
+            0.017662706,
+            0.04517666,
+            0.10499358,
+            0.19661194,
+            0.25,
+            0.19661193,
+            0.10499363,
+            0.045176655,
+            0.017662734,
+            0.006648033,
+            0.0024664658
+        ];
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn calculates_sigmoid_prime_for_2d_array() {
+        let xs = array![[-5., -4., -3., -2., -1., 0.], [1., 2., 3., 4., 5., 6.]];
+
+        let actual = Algebra::sigmoid_prime(xs);
+
+        let expected = array![
+            [
+                0.0066480567,
+                0.017662706,
+                0.04517666,
+                0.10499358,
+                0.19661194,
+                0.25
+            ],
+            [
+                0.19661193,
+                0.10499363,
+                0.045176655,
+                0.017662734,
+                0.006648033,
+                0.0024664658
+            ]
         ];
         assert_eq!(actual, expected);
     }
