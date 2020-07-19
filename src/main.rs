894925b2903a728324303d84ace3ac2d7052ef35
Kamil Duda <kamilduda01@gmail.com>
2020-07-19 19:47:19 +0200

    Add Example usage

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
-#[macro_use]
 extern crate log;
 
-use mnist_classification::infrastructure::logging;
-use mnist_classification::prelude::NeuralNetwork;
 
+use mnist_classification::infrastructure::logging;
+use mnist_classification::infrastructure::mnist_loader::*;
+
+use log::*;
+use mnist_classification::prelude::NeuralNetwork;
 
 fn main() {
     logging::init();
+    let labels_path = "resources/t10k-labels-idx1-ubyte.gz";
+    let images_path = "resources/t10k-images-idx3-ubyte.gz";
+
+    let mnist_labels = MnistDataLoader::load_labels(labels_path).unwrap();
+    let mnist_images = MnistDataLoader::load_images(images_path).unwrap();
+
+    info!("{}", mnist_labels);
+    info!("{}", mnist_images);
 
     let _x = NeuralNetwork::from(vec![5, 4, 3, 2, 1]);
 }
