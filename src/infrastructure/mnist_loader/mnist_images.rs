3211f906b9a75fa542771acd2c5da905187ca04e
Kamil Duda <kamilduda01@gmail.com>
2020-07-19 18:06:36 +0200

    Add raw mnist images and labels

---
+++ src/infrastructure/mnist_loader/mnist_images.rs
@@ -0,0 +1,38 @@
+use std::fmt::{Display, Formatter};
+
+pub struct MnistImages {
+    number_of_images: u32,
+    images: Vec<Vec<u8>>,
+}
+
+// todo: tests
+impl MnistImages {
+    pub fn new(number_of_images: u32, images: Vec<Vec<u8>>) -> MnistImages {
+        MnistImages {
+            number_of_images,
+            images,
+        }
+    }
+}
+
+impl Display for MnistImages {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "MnistImages(number_of_images: {})", self.number_of_images)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn creates_new_instance() {
+        let number_of_images = 100;
+        let images = vec![vec![0u8; 100]; 100];
+
+        let actual = MnistImages::new(number_of_images, images.clone());
+
+        assert_eq!(actual.number_of_images, number_of_images);
+        assert_eq!(actual.images, images);
+    }
+}
---
+++ src/infrastructure/mnist_loader/mnist_labels.rs
@@ -0,0 +1,37 @@
+use std::fmt::{Display, Formatter};
+
+pub struct MnistLabels {
+    number_of_labels: u32,
+    labels: Vec<u8>,
+}
+
+impl MnistLabels {
+    pub fn new(number_of_labels: u32, labels: Vec<u8>) -> MnistLabels {
+        MnistLabels {
+            number_of_labels,
+            labels,
+        }
+    }
+}
+
+impl Display for MnistLabels {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "MnistLabels(number_of_labels: {})", self.number_of_labels)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn creates_new_instance() {
+        let number_of_labels = 100;
+        let labels = vec![0u8; 100];
+
+        let actual = MnistLabels::new(number_of_labels, labels.clone());
+
+        assert_eq!(actual.number_of_labels, number_of_labels);
+        assert_eq!(actual.labels, labels);
+    }
+}
