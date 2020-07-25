b07faf7dc74eb91a191845652f56970248880e8a
Kamil Duda <kamilduda01@gmail.com>
2020-07-25 22:35:13 +0200

    Reformat

--- src/infrastructure/mnist_loader/file_reader/mod.rs
+++ src/infrastructure/mnist_loader/file_reader/mod.rs
@@ -1,8 +1,11 @@
 use std::io::Read;
 
-pub mod gz_file_reader;
 pub mod flat_file_reader;
+pub mod gz_file_reader;
 
-pub trait FileReader<T> where T: Read {
+pub trait FileReader<T>
+where
+    T: Read,
+{
     fn read(path: &str) -> Result<T, std::io::Error>;
 }
